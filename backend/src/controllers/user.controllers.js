582eb1deeded732d64f9897498fe97c62a0f6f08
Subro2005 <subrata.das5624@gmail.com>
2026-09-17 22:43:39 +0530

    folder added

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,5 @@
+import user from "../models/user.models.js"
+import {Apierror} from "../utils/Apierror.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asynchandler.js"
+
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,107 +1,111 @@
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema= new mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required:true
-        },
-        username:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        password:{
-            type:String,
-            required:true,
-            select:false
-        },
-        bio:{
-            type:String,
-            default:""
-        },
-        
-        profileimage:{
-            type:String,
-            default:""
-        },
-        profileimageppublicid:{
-            type:String,
-            default:""
-        },
-        followers:{
-           type: mongoose.Schema.Types.ObjectId,
-                ref: "user"
-        },
-        following:{
-            type: mongoose.Schema.Types.ObjectId,
-                ref: "user"
-        },
-        savedposts:{
-            type: mongoose.Schema.Types.ObjectId,
-                ref: "post"
-        },
-        refreshtokens:{
-            type:String,
-            defaut:""
-        }
-    
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    profileimage: {
+      type: String,
+      default: "",
+    },
+    profileimagepublicid: {
+      type: String,
+      default: "",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+
+    savedposts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+    refreshtokens: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
     {
-        timestamps:true
-    }
-)
+      id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
+};
 
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+};
 
-const user =mongoose.model("user",userSchema)
+const user = mongoose.model("user", userSchema);
 
-
-export default user
-
-
-userSchema.pre("save",async function(){
-    if(!isModified ("password")){
-        return next()
-    }
-    this.password=await bcrypt.hash(this.password,10)
-    next();
-
-})
-
-userSchema.methods.isPasswordCorrect=async function (password) {
-    return await bcrypt.compare(password,this.password)
-}
-
-userSchema.methods.generateAccessToken=function(){
-    return jwt.sign(
-        {
-            id:this._id,
-            username:this.username,
-            email:this.email,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: "1h",
-        },
-    )
-
-}
-
-
-userSchema.methods.generateRefreshToken=function(){
-    return jwt.sign(
-        {
-            id:this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:"7d",
-        },
-    )
-}
+export default user;
