0f4e7b13227da26517788bee53b4370694143790
Subro2005 <subrata.das5624@gmail.com>
2026-09-25 21:40:13 +0530

    added

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -6,23 +6,27 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      default:"",
+      default: "",
     },
+
     username: {
       type: String,
       required: true,
       unique: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
     },
+
     password: {
       type: String,
       required: true,
       select: false,
     },
+
     bio: {
       type: String,
       default: "",
@@ -32,10 +36,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+
     profileimagepublicid: {
       type: String,
       default: "",
     },
+
     followers: [
       {
         type: mongoose.Schema.Types.ObjectId,
@@ -56,55 +62,89 @@ const userSchema = new mongoose.Schema(
         ref: "post",
       },
     ],
+
+    // Refresh token
     refreshToken: {
       type: String,
       default: "",
     },
+
+ 
+    resetPasswordToken: {
+      type: String,
+      default: "",
+    },
+
+    
+    resetPasswordExpiry: {
+      type: Date,
+      default: null,
+    },
   },
   {
     timestamps: true,
-  },
+  }
 );
 
 
+// Hash password before saving
 userSchema.pre("save", async function (next) {
+
   if (!this.isModified("password")) {
     return next();
   }
+
   this.password = await bcrypt.hash(this.password, 10);
+
   next();
 });
 
 
+// Check password
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+
+  return await bcrypt.compare(
+    password,
+    this.password
+  );
 };
 
+
+// Generate Access Token
 userSchema.methods.generateAccessToken = function () {
+
   return jwt.sign(
     {
       id: this._id,
       username: this.username,
       email: this.email,
     },
+
     process.env.ACCESS_TOKEN_SECRET,
+
     {
       expiresIn: "1h",
-    },
+    }
   );
 };
 
+
+// Generate Refresh Token
 userSchema.methods.generateRefreshToken = function () {
+
   return jwt.sign(
     {
       id: this._id,
     },
+
     process.env.REFRESH_TOKEN_SECRET,
+
     {
       expiresIn: "7d",
-    },
+    }
   );
 };
+
 
 const User = mongoose.model("user", userSchema);
 
